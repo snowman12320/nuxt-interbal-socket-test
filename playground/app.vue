2f7a804507612ab81e81d96f435a5803849e7bfb
snowman12320 <snowman12320@gmail.com>
2024-02-18 20:56:27 +0800

    Add username assignment and display messages in app.vue

--- playground/app.vue
+++ playground/app.vue
@@ -1,27 +1,47 @@
 <template>
   <div>
     <h1>Nuxt module playground!</h1>
-    <button @click="sendMessage">Click Me To Send Message</button>
+    <ul>
+      <li v-for="(msg, index) in messages" :key="index">
+        <strong>{{ msg.user }}:</strong> {{ msg.text }}
+      </li>
+    </ul>
+
+    <form @submit.prevent="sendMessage">
+      <input v-model="newMessage" type="text" placeholder="Type a message" />
+      <button type="submit">Send</button>
+    </form>
   </div>
 </template>
 
 <script setup>
 const { $io } = useNuxtApp();
 
+const messages = ref([]); // reactive property to hold the messages
+const newMessage = ref(""); // reactive property to hold the new message
+const username = ref("玩家一"); // reactive property to hold the username
+
 const sendMessage = () => {
-  $io.emit("send message", "new message sent");
+  $io.emit("send message", { text: newMessage.value, user: username.value });
+  messages.value.push({ text: newMessage.value, user: username.value }); // push the new message into the array
+  newMessage.value = "";
 };
 
 onMounted(() => {
   $io.on("connect", () => {
-    console.log("connected", $io.id);
+    console.log("connected 123", $io.id);
   });
   $io.on("user disconnected", function (data) {
-    alert(`${data} got disconnected`);
+    // alert(`${data} got disconnected`);
+  });
+
+  $io.on("assign username", (data) => {
+    username.value = data;
   });
 
   $io.on("new message sent", (data) => {
-    alert(data);
+    // alert(data);
+    messages.value.push(data); // push the new message into the array
   });
 });
 </script>
--- playground/sockets/index.ts
+++ playground/sockets/index.ts
@@ -1,14 +1,25 @@
 import { Server, Socket } from "socket.io";
 
 export default function (io: Server) {
+  let userCount = 0;
+
   io.on("connection", (socket: Socket) => {
+    let username = userCount === 0 ? "玩家一" : "玩家二";
+    console.log(username);
+    userCount += 1;
+    socket.broadcast.emit("assign username", username);
+
+    // console.log(socket);
     socket.on("send message", (data) => {
+      // console.log(data);
       socket.broadcast.emit("new message sent", data);
     });
-    console.log("socket connected", socket.id);
+
+    console.log("socket connected 123", socket.id);
     socket.on("join room", (room) => {
       socket.join(room);
     });
+
     socket.on("disconnect", (reason) => {
       console.log("socket disconnected");
       io.emit("user disconnected", socket.id);
